becaaa440fe2afbd0d8c98cd808ca67e1c0ba55f
MohammadMazin <daniadamwolfgang@gmail.com>
2023-08-16 18:08:42 +0500

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <Toaster />
         <Navbar />
         <div className="max-w-screen-2xl ml-auto mr-auto">{children}</div>
