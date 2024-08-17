154bb2f2246781fcefe378ca14d2d21cf712ea4b
MohammadMazin <daniadamwolfgang@gmail.com>
2024-08-17 15:09:18 +0900

    remove preview AGAIN

--- app/create-gif/page.tsx
+++ app/create-gif/page.tsx
@@ -84,7 +84,7 @@ export default function CreateGif() {
     <main className="overflow-hidden mt-6">
       <div className="flex flex-col min-h-[85vh] lg:flex-row md:h-[85vh] px-4 gap-4 lg:justify-center lg:items-center">
         <section className="h-[30vh] lg:h-full w-full">
-          <Preview />
+          {/* <Preview /> */}
         </section>
 
         <section className="h-[30vh] lg:h-full w-full flex flex-col justify-start gap-8">
