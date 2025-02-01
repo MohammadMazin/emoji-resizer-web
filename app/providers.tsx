796c8f7b564069ff2b26c66afd28901acd2cdc31
Syed Mohammad Mazin <syedmohammadmazin@Syeds-MacBook-Pro.local>
2025-02-01 14:29:59 +0500

    feat(fe): posthog logs

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,6 +5,7 @@ import { PostHogProvider } from "posthog-js/react";
 if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    enable_recording_console_log: true,
   });
 }
 export function CSPostHogProvider({ children }: any) {
